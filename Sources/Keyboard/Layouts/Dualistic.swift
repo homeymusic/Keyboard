6101a249c676101a2210b7630398d345c6db6dde
Brian Mulloy <brian@landlessness.net>
2023-09-06 13:22:00 -0400

    col num relative to tonic

--- Sources/Keyboard/Layouts/Dualistic.swift
+++ Sources/Keyboard/Layouts/Dualistic.swift
@@ -5,13 +5,13 @@ struct Dualistic<Content>: View where Content: View {
     let content: (Pitch, Bool) -> Content
     var model: KeyboardModel
     var octaveCount: Int
-    let tonic = 20
+    let tonic = 32
 
     var body: some View {
         VStack(spacing: 0) {
             ForEach((0...(octaveCount - 1)).reversed(), id: \.self) { row in
                 HStack(spacing: 0) {
-                    ForEach(0...25, id: \.self) { col in
+                    ForEach(-12...12, id: \.self) { col in
                         KeyContainer(model: model,
                                      pitch: Pitch(intValue: row * 12 + col + tonic),
                                      content: content)
