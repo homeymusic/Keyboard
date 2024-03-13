5d4425a7864ebe6dc64eae046cd94c172facf30a
Brian Mulloy <brian@landlessness.net>
2024-03-13 13:08:55 -0400

    fixed the way P4, tt, and P5 rendered

--- Sources/Keyboard/Layouts/Grid.swift
+++ Sources/Keyboard/Layouts/Grid.swift
@@ -10,10 +10,10 @@ struct Grid<Content>: View where Content: View {
     var tonicPitchClass: Int
     let initialC: Int
     let doubleColumns = [1,3,8,10]
-    let skipColumns = [2,4,6,9,11]
+    let skipColumns = [2,4,6,7,9,11]
     
     func tritoneLength(_ proxySize: CGSize) -> CGFloat {
-        return min(proxySize.height * 0.3125, proxySize.width * 0.625)
+        return min(proxySize.height * 0.3125, proxySize.width * 1.0)
     }
     
     var body: some View {
@@ -40,35 +40,25 @@ struct Grid<Content>: View where Content: View {
                                                  content: content)
                                 }
                                 .padding(0)
-                            } else if (mod(col, 12) == 5 || mod(col, 12) == 7) {
-                                let step: Int = if mod(col, 12) == 5 && col > 0 {
-                                    1
-                                } else if mod(col, 12) == 7 && col < 0 {
-                                    -1
-                                } else {
-                                    0
-                                }
-                                
-                                if step != 0 {
-                                    KeyContainer(model: model,
-                                                 keyboardCell: KeyboardCell(row: row, col: col, pitch: Pitch(intValue: midi)),
-                                                 pitch: Pitch(intValue: midi),
-                                                 content: content)
-                                    KeyContainer(model: model,
-                                                 keyboardCell: KeyboardCell(row: row, col: col+step*2, pitch: Pitch(intValue: midi+step*2)),
-                                                 pitch: Pitch(intValue: midi+step*2),
-                                                 content: content)
-                                    .overlay() {
-                                        GeometryReader { proxy in
-                                            let ttLength = tritoneLength(proxy.size)
-                                            KeyContainer(model: model,
-                                                         keyboardCell: KeyboardCell(row: row, col: col + 1, pitch: Pitch(intValue: midi + step)),
-                                                         pitch: Pitch(intValue: midi + step),
-                                                         zIndex: 1,
-                                                         content: content)
-                                            .frame(width: ttLength, height: ttLength)
-                                            .offset(x: -ttLength / 2.0, y: proxy.size.height / 2.0 - ttLength / 2.0)
-                                        }
+                            } else if (mod(col, 12) == 5) {
+                                KeyContainer(model: model,
+                                             keyboardCell: KeyboardCell(row: row, col: col, pitch: Pitch(intValue: midi)),
+                                             pitch: Pitch(intValue: midi),
+                                             content: content)
+                                KeyContainer(model: model,
+                                             keyboardCell: KeyboardCell(row: row, col: col+2, pitch: Pitch(intValue: midi+2)),
+                                             pitch: Pitch(intValue: midi+2),
+                                             content: content)
+                                .overlay() {
+                                    GeometryReader { proxy in
+                                        let ttLength = tritoneLength(proxy.size)
+                                        KeyContainer(model: model,
+                                                     keyboardCell: KeyboardCell(row: row, col: col + 1, pitch: Pitch(intValue: midi + 1)),
+                                                     pitch: Pitch(intValue: midi + 1),
+                                                     zIndex: 1,
+                                                     content: content)
+                                        .frame(width: ttLength, height: ttLength)
+                                        .offset(x: -ttLength / 2.0, y: proxy.size.height / 2.0 - ttLength / 2.0)
                                     }
                                 }
                             } else if (skipColumns.contains(mod(col, 12))) {
